a71bf17e7d3750fb7a38b740e744fb60adccbaed
Daijoubu22 <alextrio.2224@gmail.com>
2023-02-12 21:01:36 +0300

    use socketjs

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from '@layout/Header/Header';
 import Footer from '@layout/Footer/Footer';
+import TestComponent from '@shared/TestComponent/TestComponent';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <div className="app__content container">
-        Hello world!
+        <TestComponent />
       </div>
       <Footer />
     </div>
