75d1405e164d4ab08177914c8e7fc31e018f5f3a
Daijoubu22 <alextrio.2224@gmail.com>
2023-02-13 21:48:33 +0300

    add bearing icons

--- frontend/src/services/constants.ts
+++ frontend/src/services/constants.ts
@@ -1,6 +1,6 @@
 export enum Colors {
-  RED = 'rgb(255,46,46)',
-  ORANGE = 'rgb(255,157,46)',
+  RED = 'rgb(255,18,18)',
+  ORANGE = 'rgb(255,135,0)',
   RED_TRANSPARENT = 'rgba(255,46,46,0.5)',
   ORANGE_TRANSPARENT = 'rgba(255,157,46,0.5)',
   GREEN = '#2bde00',
