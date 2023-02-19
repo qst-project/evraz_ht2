ed6db8494a514983ab79902dd51b8dd806d78dd1
vladt <trocenko.vlad@mail.ru>
2023-02-19 07:52:17 +0300

    fix paging

--- backend/src/main/java/org/qst/evrazht2backend/controller/NotificationController.java
+++ backend/src/main/java/org/qst/evrazht2backend/controller/NotificationController.java
@@ -1,11 +1,12 @@
 package org.qst.evrazht2backend.controller;
 
-import jakarta.validation.constraints.NotNull;
 import org.qst.evrazht2backend.repository.Notification;
 import org.qst.evrazht2backend.repository.NotificationRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +18,8 @@ public class NotificationController {
     }
 
     @GetMapping("/notifications")
-    public Page<Notification> notifications(@NotNull final Pageable pageable) {
-        return notificationRepository.findAll(pageable);
+    public Page<Notification> notifications(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "3") int size) {
+        Pageable paging = PageRequest.of(page, size);
+        return notificationRepository.findAll(paging);
     }
 }
