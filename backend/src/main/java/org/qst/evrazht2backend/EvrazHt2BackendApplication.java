2c84239a2603e6f56c02c02669b1461a6b06e32e
vladt <trocenko.vlad@mail.ru>
2023-02-19 05:00:36 +0300

    fix backend build

--- backend/src/main/java/org/qst/evrazht2backend/EvrazHt2BackendApplication.java
+++ backend/src/main/java/org/qst/evrazht2backend/EvrazHt2BackendApplication.java
@@ -2,8 +2,6 @@ package org.qst.evrazht2backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.transaction.PlatformTransactionManagerCustomizer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EvrazHt2BackendApplication {
